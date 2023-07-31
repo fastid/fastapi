0c2865fac363003686404c48f616144cb51e8aa2
kostya <kostya@yandex.ru>
2023-07-31 18:59:47 +0300

    refactor: Remove SessionID

--- fastid/typing.py
+++ fastid/typing.py
@@ -1,11 +1,9 @@
 from typing import NewType
-from uuid import UUID
 
 from pydantic import EmailStr
 
 UserID = NewType('UserID', int)
 ProfileID = NewType('ProfileID', int)
 Phone = NewType('Phone', int)
-SessionID = NewType('SessionID', UUID)
 Email = NewType('Email', EmailStr)
 Password = NewType('Password', str)
