52c7811ed610e25413c21dc948ca14c1d84f15cc
kostya <kostya@yandex.ru>
2023-07-31 16:46:20 +0300

    ci: Add codeql

--- fastid/typing.py
+++ fastid/typing.py
@@ -1,10 +1,11 @@
 from typing import NewType
+from uuid import UUID
 
 from pydantic import EmailStr
 
 UserID = NewType('UserID', int)
 ProfileID = NewType('ProfileID', int)
 Phone = NewType('Phone', int)
-SessionID = NewType('SessionID', int)
+SessionID = NewType('SessionID', UUID)
 Email = NewType('Email', EmailStr)
 Password = NewType('Password', str)
