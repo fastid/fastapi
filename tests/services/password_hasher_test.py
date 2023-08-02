732cdc3f6a4bdf5b0c5b043b64e736a820b76368
kostya <kostya@yandex.ru>
2023-08-02 15:51:10 +0300

    feat: Added password verify

--- tests/services/password_hasher_test.py
+++ tests/services/password_hasher_test.py
@@ -1,6 +1,9 @@
 from unittest.mock import PropertyMock, patch
 
+import pytest
+
 from fastid import services, typing
+from fastid.exceptions import InternalServerException
 from fastid.settings import PasswordHasherMemoryProfile
 
 
@@ -15,3 +18,13 @@ async def test_hasher_password_hasher_memory_profile_low():
 async def test_hasher_password_hasher_memory_profile_high():
     password_string = await services.password_hasher.hasher(password=typing.Password('test'))
     assert password_string
+
+
+async def test_verify():
+    password_hash = await services.password_hasher.hasher(password=typing.Password('test'))
+    assert await services.password_hasher.verify(password_hash=password_hash, password=typing.Password('test'))
+
+
+async def test_verify_fail():
+    with pytest.raises(InternalServerException):
+        await services.password_hasher.verify(password_hash='1234', password=typing.Password('test'))
