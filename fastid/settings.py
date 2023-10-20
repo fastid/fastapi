dc8f5516776dc9acd819bf636a3441ef0bbb8767
kostya <kostya@yandex.ru>
2023-10-20 10:41:49 +0300

    pref: Added sentry integration

--- fastid/settings.py
+++ fastid/settings.py
@@ -2,7 +2,7 @@ import os
 from enum import Enum
 from pathlib import Path
 
-from pydantic import SecretStr
+from pydantic import HttpUrl, SecretStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -159,6 +159,9 @@ class Settings(BaseSettings):
 
     mime_idstring: str = 'fastid'
     """ Sets the identifier for sending emails """
+
+    sentry_dsn: HttpUrl | None = None
+    """ Adds integration with sentry """
 
     model_config = SettingsConfigDict(
         env_file=f'{base_dir}/.env',
