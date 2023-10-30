60d074fd36cb116097f55595adaf7ff6bd4dd392
kostya <kostya@yandex.ru>
2023-10-30 14:44:20 +0300

    fix: style

--- fastid/services/config.py
+++ fastid/services/config.py
@@ -5,8 +5,6 @@ from . import models
 
 @decorator_trace(name='services.config.get')
 async def get() -> models.Config:
-    print(settings.captcha)
-
     return models.Config(
         captcha=settings.captcha,
         captcha_usage=settings.captcha_usage.split(',') if settings.captcha_usage else [],
@@ -15,17 +13,3 @@ async def get() -> models.Config:
         password_policy_min_length=settings.password_policy_min_length,
         password_policy_max_length=settings.password_policy_max_length,
     )
-
-    # captcha_usage = []
-    # for obj in config_repo:
-    #     if obj.key == 'is_setup':
-    #         config.is_setup = True
-    #     elif obj.key == 'captcha':
-    #         config.captcha = obj.value
-    #     elif obj.key == 'recaptcha_site_key':
-    #         config.recaptcha_site_key = obj.value
-    #     elif obj.key == 'captcha_usage':
-    #         captcha_usage.append(obj.value)
-
-    # if captcha_usage:
-    #     config.captcha_usage = captcha_usage
